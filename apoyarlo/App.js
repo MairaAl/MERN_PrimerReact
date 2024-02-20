9c9d9959261fb74cd0051a93db788e866de44d5c
Maira Al <122545402+MairaAl@users.noreply.github.com>
2024-02-20 20:52:04 -0300

    First commit

---
+++ apoyarlo/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hey Dojo!</h1>
+        <h3>Things I need to do:</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
